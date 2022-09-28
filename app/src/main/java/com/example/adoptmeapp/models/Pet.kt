296ecdf86f8201f50492b9207b53c235f02a5db4
maxwellpv <55470371+maxwellpv@users.noreply.github.com>
2022-09-27 22:07:46 -0500

    Feature: Pet artifacts create and first implementation

---
+++ app/src/main/java/com/example/adoptmeapp/models/Pet.kt
@@ -0,0 +1,15 @@
+package com.example.adoptmeapp.models
+
+class Pet (
+    var id: Int,
+    var type: String,
+    var name: String,
+    var attention: String,
+    var age: Int,
+    var race: String,
+    var isAdopted: Boolean,
+    var isPublished: Boolean,
+    var gender: String,
+    var urlToImage: String,
+    var userId: String
+)

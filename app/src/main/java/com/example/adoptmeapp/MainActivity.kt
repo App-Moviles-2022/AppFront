3be9dd37b998533a57e9f3e9db1cfe136b706518
jack <jack008007z@outlook.es>
2022-09-29 11:41:54 -0500

    feat(adopter intent)

--- app/src/main/java/com/example/adoptmeapp/MainActivity.kt
+++ app/src/main/java/com/example/adoptmeapp/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.adoptmeapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.adoptmeapp.controllers.activities.SignInActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+    fun appInitSesion(view: View){
+        val intent= Intent(this, SignInActivity::class.java).apply {  }
+        startActivity(intent)
     }
 }

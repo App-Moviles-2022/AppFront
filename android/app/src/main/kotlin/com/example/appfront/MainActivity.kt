92289c43407af90c05e4c4665e3daa6e329bb128
maxwellpv <55470371+maxwellpv@users.noreply.github.com>
2022-10-25 11:01:42 -0500

    'a'

---
+++ android/app/src/main/kotlin/com/example/appfront/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.appfront
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'dart:html';
+
+import 'package:adoptme_app/modulos/gestion-usuario/perfil-usuario.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AdoptmeStless());
+}
+
+class AdoptmeStless extends StatelessWidget {
+  const AdoptmeStless({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter App",
+      theme: ThemeData(
+          primarySwatch:Colors.indigo
+      ),
+      home: AdoptmeStull(),
+    );
+  }
+}
+
+class AdoptmeStull extends StatefulWidget {
+  const AdoptmeStull({Key? key}) : super(key: key);
+  @override
+  State<AdoptmeStull> createState() => _AdoptmeStullState();
+}
+
+class _AdoptmeStullState extends State<AdoptmeStull> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter App"),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ElevatedButton(onPressed: (){
+            Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const PerfilUsuario())
+            );
+          }, child: Text("Perfil de Usuario")),
+          ElevatedButton(onPressed: (){
+            Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const PerfilUsuario())
+            );
+          }, child: Text("Mis Publicaciones")),
+          ElevatedButton(onPressed: (){
+            Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const PerfilUsuario())
+            );
+          }, child: Text("Publicaciones General")),
+          ElevatedButton(onPressed: (){
+            Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const PerfilUsuario())
+            );
+          }, child: Text("Mis Anuncios")),
+          ElevatedButton(onPressed: (){
+            Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const PerfilUsuario())
+            );
+          }, child: Text("Anuncios General")),
+          ElevatedButton(onPressed: (){
+            Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const PerfilUsuario())
+            );
+          }, child: Text("Subscripciones")),
+          ElevatedButton(onPressed: (){
+            Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const PerfilUsuario())
+            );
+          }, child: Text("Mis Mascotas"))
+        ],
+      ),
+    );
+  }
+}
